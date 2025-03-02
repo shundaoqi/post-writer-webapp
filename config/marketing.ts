df327f95fa4938619e78efa7d0ec07982acc9d8c
shun <shundaoqi@gmail.com>
2025-03-02 16:33:55 +0900

    create top page

---
+++ config/marketing.ts
@@ -0,0 +1,18 @@
+import { MarkethingConfig } from "@/types";
+
+export const marketnigConfig: MarkethingConfig = {
+  mainNav: [
+    {
+      title: '特徴', 
+      href: '#features'
+    },
+    {
+      title: 'ブログ',
+      href:'/blog'
+    },
+    {
+      title: '価格',
+      href: '/pricing'
+    }
+  ]
+}
